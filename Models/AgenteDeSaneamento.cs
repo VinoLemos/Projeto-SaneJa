1aa0f5f5c73a86b636e3e3282b63cacb464c1512
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-09-08 22:19:24 -0300

    Annotations

--- Models/AgenteDeSaneamento.cs
+++ Models/AgenteDeSaneamento.cs
@@ -1,14 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Projeto_SaneJa.Models
 {
+    [Table("Agentes")]
     public class AgenteDeSaneamento : IUsuario
     {
+        [Key]
         public int Matricula { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório!")]
+        [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres", MinimumLength = 5)]
         public string? Nome { get; set; }
+        [Required(ErrorMessage = "RG é obrigatório!")]
+        [StringLength(9)]
         public string? Rg { get; set; }
+        [Required]
         public long Cpf{ get; set; }
         public DateTime DataNascimento { get; set; }
+        [Required(ErrorMessage = "Telefone é obrigatório")]
         public int Telefone { get; set; }
+        [Required]
         public string? Login { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
         public string? Senha { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
--- Models/Imovel.cs
+++ Models/Imovel.cs
@@ -1,15 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 namespace Projeto_SaneJa.Models
 {
+    [Table("Imoveis")]
     public class Imovel
     {
+        [Required(ErrorMessage = "Nome da rua obrigatório")]
+        [StringLength(80, ErrorMessage = "Deve ter no máximo 80 caracteres")]
         public string? Rua { get; set; }
         public int Numero { get; set; }
+        [StringLength(50)]
         public string? Complemento { get; set; }
+        [Required]
         public string? Bairro { get; set; }
+        [Required]
         public int Cep { get; set; }
+        [Required]
+        [StringLength(30)]
         public string? Cidade { get; set; }
+        [Key]
         public int Rgi { get; set; }
+        [Required(ErrorMessage = "RGI é obrigatório")]
         public int Hidrometro { get; set; }
+        [JsonIgnore]
         public Cliente? Proprietario { get; set; }
     }
 }
