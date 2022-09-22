e9514f0e7004edd9987f1e8d6d0074efa9824cad
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-09-22 20:52:50 -0300

    Projeto ReactJS

---
+++ Front/saneja/src/App.js
@@ -0,0 +1,14 @@
+import saneja from './saneja.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <img src={saneja} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
