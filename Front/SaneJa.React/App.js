2badd3bcb398bc33ef38d820985550e313b61aea
Sabrina Maria Andrade Carvalho <sabri.andrade99@gmail.com>
2022-09-22 15:55:02 -0300

    reestruturacao do front

---
+++ Front/SaneJa.React/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet, Image } from 'react-native'
+
+export default () => {
+
+  return(
+    <SafeAreaView style={style.App}>
+      <Image source={require('./MicrosoftTeams-image.png')}/>
+      <Text style={style.Titulo}>Hello World!!</Text>
+
+    </SafeAreaView>
+  )
+  
+
+}
+
+const style = StyleSheet.create({
+  App: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
+  },
+
+  Titulo: {
+    fontSize: 35,
+    fontWeight: 'bold'
+  }
+
+})
