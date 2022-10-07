6c9ffc5872ef04ee01a30faec06108d6485edf49
Talita-S <talitassantos2015@gmail.com>
2022-10-07 16:06:42 -0300

    Estilizando o index

--- Front/saneja/src/pages/Index.js
+++ Front/saneja/src/pages/Index.js
@@ -15,7 +15,7 @@ function Index() {
 
             <div>
                 <p>Não tem uma conta?</p>
-                <p>Cadastre-se</p>
+                <a href='#'>Cadastre-se</a>
             </div>
         </div>
     )
