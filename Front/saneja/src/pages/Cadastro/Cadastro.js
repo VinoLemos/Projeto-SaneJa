051a03a115cb14f74a3df27e741eb2073584f3f8
Talita-S <talitassantos2015@gmail.com>
2022-10-18 17:51:31 -0300

    Mensagem de cadastro e redirecionamento para a pagina de login

--- Front/saneja/src/pages/Cadastro/Cadastro.js
+++ Front/saneja/src/pages/Cadastro/Cadastro.js
@@ -10,6 +10,8 @@ import * as yup from "yup";
 
 import InputMask from 'react-input-mask';
 
+import swal from 'sweetalert';
+
 const schema = yup.object({
     nome: yup.string().required('Nome é obrigatório'),
     cpf: yup.string().required('CPF é obrigatório').min(11, 'O CPF precisa ter 11 dígitos'),
@@ -27,19 +29,25 @@ function Cadastro() {
         resolver: yupResolver(schema)
     })
 
-    const onSubmit = data => console.log(data);
+    const handleCreateUser = () => {
+        swal({
+            title: "Cadastro realizado com sucesso!",
+            text: "Verifique seu email para confirmar seu cadastro",
+            button: "Ir para a página de login" 
+        }).then(() => {window.location.href = ('/login')});
+    }
 
     return (
         <div className="main-container">
             <Header/>
-            <div id='alerta'></div>
+            <div id='alerta-campos'></div>
             <div className="form-cadastro">
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(handleCreateUser)}>
                     <h1>Dados Pessoais</h1>
                     <input type="text" name="nome" {...register("nome", { required: true })} placeholder="Nome Completo" className="form-data-input"
                         aria-invalid={errors.nome ? "true" : "false"}
                     /> <br/>
-                    {errors.nome && <span className='alerta'>{errors.nome?.message}</span> } <br/> 
+                    {errors.nome && <span className='alerta-campos'>{errors.nome?.message}</span> } <br/> 
 
                     <InputMask className="form-data-input"
                         name="cpf"
@@ -48,7 +56,7 @@ function Cadastro() {
                         mask="999.999.999-99"
                         aria-invalid={errors.cpf ? "true" : "false"}
                     /> <br/> 
-                    {errors.cpf && <span className='alerta'>{errors.cpf?.message}</span> } <br/>
+                    {errors.cpf && <span className='alerta-campos'>{errors.cpf?.message}</span> } <br/>
 
                     <InputMask className="form-data-input"
                         name="rg"
@@ -57,13 +65,13 @@ function Cadastro() {
                         placeholder="RG"
                         aria-invalid={errors.rg ? "true" : "false"}
                     /> <br/>
-                    {errors.rg && <span className='alerta'>{errors.rg?.message}</span> } <br/>
+                    {errors.rg && <span className='alerta-campos'>{errors.rg?.message}</span> } <br/>
                     
                     <label>Data Nascimento</label> <br/>
                     <input type="date" name="dataNasc" {...register("dataNasc", { required: true })} className="form-data-input"
                         aria-invalid={errors.dataNasc? "true" : "false"}        
                     /> <br/>
-                    {errors.dataNasc && <span className='alerta'>{errors.dataNasc?.message}</span> } <br/>
+                    {errors.dataNasc && <span className='alerta-campos'>{errors.dataNasc?.message}</span> } <br/>
                     
                     <InputMask className="form-data-input"
                         name="telefone"
@@ -72,23 +80,23 @@ function Cadastro() {
                         placeholder="Telefone"
                         aria-invalid={errors.telefone ? "true" : "false"}
                     /> <br/>
-                    {errors.telefone && <span className='alerta'>{errors.telefone?.message}</span> } <br/>
+                    {errors.telefone && <span className='alerta-campos'>{errors.telefone?.message}</span> } <br/>
                    
 
                     <input type="text" name="email" {...register("email", { required: true })} placeholder="Email" className="form-data-input"
                         aria-invalid={errors.email? "true" : "false"}
                     /> <br/>
-                    {errors.email && <span className='alerta'>{errors.email?.message}</span> } <br/>
+                    {errors.email && <span className='alerta-campos'>{errors.email?.message}</span> } <br/>
                     
                     <input type="password" name="senha" {...register("senha", { required: true })} placeholder="Senha" className="form-data-input"
                         aria-invalid={errors.senha? "true" : "false"}
                     /> <br/>
-                    {errors.senha && <span className='alerta'>{errors.senha?.message}</span> } <br/>
+                    {errors.senha && <span className='alerta-campos'>{errors.senha?.message}</span> } <br/>
 
                     <input type="password" name="confirmarSenha" {...register("confirmarSenha", { required: true })} placeholder="Confirmar Senha" className="form-data-input"
                         aria-invalid={errors.confirmarSenha? "true" : "false"}
                     /> <br/>
-                    {errors.confirmarSenha && <span className='alerta'>{errors.confirmarSenha?.message}</span> } <br/>
+                    {errors.confirmarSenha && <span className='alerta-campos'>{errors.confirmarSenha?.message}</span> } <br/>
                     
                     <div className='div-botao'>
                         <input type="submit" value="Cadastrar" className='botao-cadastro' />
