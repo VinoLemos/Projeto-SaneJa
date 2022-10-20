eab44e80fd1d1f36ff7af73536456c7a96ba0bbb
Talita-S <talitassantos2015@gmail.com>
2022-10-20 16:56:34 -0300

    Reestruturação do cadastro

--- Front/saneja/src/pages/Cadastro/Cadastro.js
+++ Front/saneja/src/pages/Cadastro/Cadastro.js
@@ -1,112 +1,139 @@
-import React from 'react';
-
+import { useState } from 'react';
 import './Cadastro.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
-
 import InputMask from 'react-input-mask';
-
+import {Link, useNavigate} from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import swal from 'sweetalert2';
 
-const schema = yup.object({
-    nome: yup.string().required('Nome é obrigatório'),
-    cpf: yup.string().required('CPF é obrigatório').min(11, 'O CPF precisa ter 11 dígitos'),
-    rg: yup.string().required('RG é obrigatório').min(9, 'O RG precisa ter 9 dígitos'),
-    dataNasc: yup.string().required('Data de nasc. obrigatória'),
-    telefone: yup.string().required('Telefone é obrigatório'),
-    email: yup.string().email('Digite um email válido').required('Email é obrigatório'),
-    senha: yup.string().required('Senha é obrigatória').min(8, 'Senha deve conter no mín. 8 dígitos'),
-    confirmarSenha: yup.string().required('Confirmar a senha é obrigatório').oneOf([yup.ref('senha')], 'As senhas devem ser iguais'),
-}).required();
+const Cadastro = () => {
 
-function Cadastro() {
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [rg, setRg] = useState("");
+    const [dataNasc, setDataNasc] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [senhaConf, setSenhaConf] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema)
-    })
+    const {signup} = useAuth();
 
-    const handleCreateUser = () => {
+    const handleSignup = () => {
+        if (!nome | !cpf | !rg | !dataNasc | !telefone | !email | !senha) {
+            setError("Preencha todos os campos");
+            return;
+        } else if (senha !== senhaConf) {
+            setError("As senhas devem ser iguais");
+            return;
+        }
+
+        const res = signup(email, senha);
+
+        if (res) {
+            setError(res);
+            return;
+        }
+
         swal.fire({
             icon: 'success',
             title: "Cadastro realizado com sucesso!",
-            text: "Verifique seu email para confirmar o seu cadastro",
+            confirmButtonText: "Ir para página de login",
             confirmButtonColor: "#6F9CB5"
         })
+        navigate("/login");
     }
 
     return (
         <div className="main-container">
             <Header/>
-            <div id='alerta-campos'></div>
             <div className="form-cadastro">
-                <form onSubmit={handleSubmit(handleCreateUser)}>
+                <form >
                     <h1>Dados Pessoais</h1>
-                    <input type="text" name="nome" {...register("nome", { required: true })} placeholder="Nome Completo" className="form-data-input"
-                        aria-invalid={errors.nome ? "true" : "false"}
-                    /> <br/>
-                    {errors.nome && <span className='alerta-campos'>{errors.nome?.message}</span> } <br/> 
+                    <input 
+                        type="text" 
+                        value={nome} 
+                        onChange={(e) => [setNome(e.target.value), setError("")]}
+                        placeholder="Nome Completo" 
+                        className="form-data-input"
+                    /> 
 
                     <InputMask className="form-data-input"
-                        name="cpf"
-                        {...register("cpf", { required: true })}
+                        type="text" 
+                        value={cpf}
+                        onChange={(e) => [setCpf(e.target.value), setError("")]}
                         placeholder="CPF"
                         mask="999.999.999-99"
-                        aria-invalid={errors.cpf ? "true" : "false"}
-                    /> <br/> 
-                    {errors.cpf && <span className='alerta-campos'>{errors.cpf?.message}</span> } <br/>
+                    /> 
 
                     <InputMask className="form-data-input"
-                        name="rg"
+                        type="text" 
+                        value={rg}
+                        onChange={(e) => [setRg(e.target.value), setError("")]}
                         mask="99.999.999-9"
-                        {...register("rg", { required: true })}
                         placeholder="RG"
-                        aria-invalid={errors.rg ? "true" : "false"}
-                    /> <br/>
-                    {errors.rg && <span className='alerta-campos'>{errors.rg?.message}</span> } <br/>
+                    /> 
                     
                     <label>Data Nascimento</label> <br/>
-                    <input type="date" name="dataNasc" {...register("dataNasc", { required: true })} className="form-data-input"
-                        aria-invalid={errors.dataNasc? "true" : "false"}        
-                    /> <br/>
-                    {errors.dataNasc && <span className='alerta-campos'>{errors.dataNasc?.message}</span> } <br/>
+                    <input 
+                        type="date" 
+                        value={dataNasc} 
+                        onChange={(e) => [setDataNasc(e.target.value), setError("")]}
+                        className="form-data-input"
+                    /> 
+                    
                     
                     <InputMask className="form-data-input"
-                        name="telefone"
+                        value={telefone}
+                        onChange={(e) => [setTelefone(e.target.value), setError("")]}
                         mask="(99) 99999-9999"
-                        {...register("telefone", { required: true })}
                         placeholder="Telefone"
-                        aria-invalid={errors.telefone ? "true" : "false"}
-                    /> <br/>
-                    {errors.telefone && <span className='alerta-campos'>{errors.telefone?.message}</span> } <br/>
-                   
-
-                    <input type="text" name="email" {...register("email", { required: true })} placeholder="Email" className="form-data-input"
-                        aria-invalid={errors.email? "true" : "false"}
-                    /> <br/>
-                    {errors.email && <span className='alerta-campos'>{errors.email?.message}</span> } <br/>
+                    /> 
                     
-                    <input type="password" name="senha" {...register("senha", { required: true })} placeholder="Senha" className="form-data-input"
-                        aria-invalid={errors.senha? "true" : "false"}
-                    /> <br/>
-                    {errors.senha && <span className='alerta-campos'>{errors.senha?.message}</span> } <br/>
+                    <input 
+                        type="text" 
+                        value={email} 
+                        onChange={(e) => [setEmail(e.target.value), setError("")]}
+                        placeholder="Email" 
+                        className="form-data-input"
+                    /> 
+                    
+                    <input 
+                        type="password" 
+                        value={senha} 
+                        onChange={(e) => [setSenha(e.target.value), setError("")]}
+                        placeholder="Senha" 
+                        className="form-data-input"
+                    />
 
-                    <input type="password" name="confirmarSenha" {...register("confirmarSenha", { required: true })} placeholder="Confirmar Senha" className="form-data-input"
-                        aria-invalid={errors.confirmarSenha? "true" : "false"}
-                    /> <br/>
-                    {errors.confirmarSenha && <span className='alerta-campos'>{errors.confirmarSenha?.message}</span> } <br/>
+                    <input 
+                        type="password" 
+                        value={senhaConf} 
+                        onChange={(e) => [setSenhaConf(e.target.value), setError("")]}
+                        placeholder="Confirmar senha" 
+                        className="form-data-input"
+                    />
+
+                    <span className='alerta-campos'>{error}</span>
                     
                     <div className='div-botao'>
-                        <input type="submit" value="Cadastrar" className='botao-cadastro' />
+                        <input type="button" value="Cadastrar" className='botao-cadastro' onClick={handleSignup} />
                     </div>
+                    <div className='link-login'>
+                        <Link to="/login">
+                            <a href='/login'>Já tem uma conta?</a>
+                        </Link>
+                    </div>
+                    
                 </form>
             </div>
             <Footer/>
         </div>
     )
-}
+};
 
 export default Cadastro
