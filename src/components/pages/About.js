93e4ee78d322da102f783a7588b339b413e4b7b0
Talita-S <talitassantos2015@gmail.com>
2023-03-01 14:23:18 -0300

    Ajuste no texto do subitítulo

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,15 +4,17 @@ import HeroSection from "../layout/HeroSection";
 
 function About() {
   return (
-    <HeroSection
-      title="Nós"
-      subtitle="Somos uma solução pensada para facilitar o processo de adequação de imóvel para que as pessoas possam receber saneamento básico"
-      paragraph="Em nossa plataforma será possível que você faça seu cadastro, informe
+    <section>
+      <HeroSection
+        title="Nós"
+        subtitle="Somos uma solução para facilitar o acesso ao saneamento básico"
+        paragraph="Em nossa plataforma será possível que você faça seu cadastro, informe
       o seu imóvel e agende visitas técnicas com nossos especialistas para
       que a análise seja feita e tudo esteja certo para que você receba a
       sua rede de saneamento básico,"
-      span="sem dor de cabeça!"
-    />
+        span="sem dor de cabeça!"
+      />
+    </section>
   );
 }
 
