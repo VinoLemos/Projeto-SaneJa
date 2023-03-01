f86b4916cf6734993e0e1d833acd4839146f3044
Talita-S <talitassantos2015@gmail.com>
2023-03-01 10:41:59 -0300

    Removendo o componente Layout

--- src/components/layout/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Container from "react-bootstrap/Container";
-
-function Layout(props) {
-  return <Container>{props.children}</Container>;
-}
-
-export default Layout;
