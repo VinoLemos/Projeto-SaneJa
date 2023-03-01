3d4c29969580b731729a06449b75a0f64983a10e
Talita-S <talitassantos2015@gmail.com>
2023-03-01 13:16:10 -0300

    Ajuste na exibição da página

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,15 @@ import React from "react";
 import HeroSection from "../layout/HeroSection";
 
 function Home() {
-  return <HeroSection />;
+  return (
+    <HeroSection
+      title="Sanejá"
+      subtitle="A sua plataforma de atendimento"
+      paragraph="Aqui você encontrará o passo a passo para fazer o adequamento do seu
+    imóvel e, assim, receber sua rede de"
+      span="saneamento básico"
+    />
+  );
 }
 
 export default Home;
