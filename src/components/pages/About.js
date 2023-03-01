408d991a07c27f0db24e1cd0df43a66ef76b00d7
Talita-S <talitassantos2015@gmail.com>
2023-03-01 14:13:11 -0300

    Criando a página Sobre

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import HeroSection from "../layout/HeroSection";
+
 function About() {
-  return <div>Sobre</div>;
+  return (
+    <HeroSection
+      title="Nós"
+      subtitle="Somos uma solução pensada para facilitar o processo de adequação de imóvel para que as pessoas possam receber saneamento básico"
+      paragraph="Em nossa plataforma será possível que você faça seu cadastro, informe
+      o seu imóvel e agende visitas técnicas com nossos especialistas para
+      que a análise seja feita e tudo esteja certo para que você receba a
+      sua rede de saneamento básico,"
+      span="sem dor de cabeça!"
+    />
+  );
 }
 
 export default About;
