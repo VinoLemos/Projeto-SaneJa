91c494ea74e9bff463dcdef96206896629dcbf45
Talita-S <talitassantos2015@gmail.com>
2023-03-04 07:16:11 -0300

    Adicionando botão de cadastro

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,23 @@ import HeroSection from "../layout/HeroSection";
 
 function Home() {
   return (
-    <HeroSection
-      title="Sanejá"
-      subtitle="A sua plataforma de atendimento"
-      paragraph="Aqui você encontrará o passo a passo para fazer o adequamento do seu
-    imóvel e, assim, receber sua rede de"
-      span="saneamento básico"
-    />
+    <>
+      <HeroSection
+        title="Sanejá"
+        subtitle="A sua plataforma de atendimento"
+        paragraph="A ferramenta certa para fazer o adequamento do seu
+    imóvel e te ajudar a receber a rede de"
+        span="saneamento básico"
+        btnText="CADASTRO"
+        btnStyle="btn--primary"
+        btnSize="btn--large"
+        link="/cadastro"
+        btnText2="LOGIN"
+        btnStyle2="btn--primary"
+        btnSize2="btn--large"
+        link2="/login"
+      />
+    </>
   );
 }
 
