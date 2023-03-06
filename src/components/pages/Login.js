0a04d57a20c9a4eab11405e15b588d2ad63853e2
Talita-S <talitassantos2015@gmail.com>
2023-03-06 11:24:19 -0300

    Validando campos no form de login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Row, Col, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import water from "../../img/pexels-water.mp4";
 import logo from "../../img/logo.png";
@@ -12,8 +14,9 @@ const Styles = styled.div`
     place-items: center center;
   }
 
-  #form-container {
-    max-width: 350px;
+  img {
+    margin-top: 6em;
+    margin-bottom: 2em;
   }
 
   h1 {
@@ -30,6 +33,8 @@ const Styles = styled.div`
   }
 
   input {
+    width: 100%;
+    color: #555;
     border-radius: 3px;
     padding: 12px;
     background-color: rgba(94, 109, 110, 0.2);
@@ -43,9 +48,41 @@ const Styles = styled.div`
     box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1),
       0 0 8px rgba(94, 109, 110, 1);
   }
+
+  p {
+    color: #555;
+    font-weight: 500;
+    font-size: 0.6em;
+    text-align: right;
+  }
+
+  svg {
+    font-size: 1.5em;
+    color: #648281;
+    cursor: pointer;
+  }
+
+  a {
+    color: #648281;
+    text-decoration: none;
+  }
+
+  span {
+    display: flex;
+    color: #bf6560;
+    margin-bottom: 0.5em;
+  }
 `;
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Styles>
       <Row>
@@ -53,33 +90,57 @@ function Login() {
           <video src={water} autoPlay loop muted />
         </Col>
         <Col xs={8}>
-          <Container id="main-container" className="d-grid h-100 text-center">
-            <Form id="form-container">
-              <img src={logo} alt="Logo Sanejá" className="mb-4 mt-4" />
+          <Container id="main-container" className="d-grid text-center ">
+            <Form>
+              <img src={logo} alt="Logo Sanejá" />
               <h1 className="fs-3 mb-4">Faça o login</h1>
+
               <Form.Group>
                 <Form.Control
-                  className="mb-3"
+                  className="mb-2"
                   type="email"
+                  name="email"
                   size="lg"
                   placeholder="Email"
                   autoComplete="username"
+                  {...register("email", {
+                    required: "Email obrigatório",
+                    pattern: {
+                      value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+                      message: "Email incompleto ou inválido",
+                    },
+                  })}
                 />
+                {errors.email && <span>{errors.email.message}</span>}
               </Form.Group>
 
               <Form.Group>
                 <Form.Control
-                  className="mb-3"
+                  className="mb-2"
                   type="password"
+                  name="senha"
                   size="lg"
                   placeholder="Senha"
                   autoComplete="current-password"
+                  {...register("senha", {
+                    required: "Senha obrigatória",
+                    minLength: {
+                      value: 4,
+                      message: "Senha precisa ter entre 4 e 8 caracteres",
+                    },
+                  })}
                 />
+                {errors.senha && <span>{errors.senha.message}</span>}
               </Form.Group>
+              <p>
+                Ainda não tem conta? <Link to="/">Cadastre-se</Link>
+              </p>
+
               <SubmitButton
                 text="Entrar"
                 btnStyle="btn--secondary"
                 btnSize="btn--x-large"
+                onClick={handleSubmit(onSubmit)}
               />
             </Form>
           </Container>
