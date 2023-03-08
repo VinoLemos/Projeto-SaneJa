892718c0026640e294eb4e8042e62e7184a564f2
Talita Santos <talita-s@ibm.com>
2023-03-08 11:54:25 -0300

    Ajsute para receber estilização do mui e a fonte roboto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import App from "./App";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>
 );
