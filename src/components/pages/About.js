3e9ef0227b5f909d340cd9c297f9685a6c919e7a
Talita Santos <talita-s@ibm.com>
2023-03-08 12:00:31 -0300

    Melhorias na página Sobre

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,9 @@ import HeroSection from "../layout/HeroSection";
 
 function About() {
   return (
-    <section>
+    <>
       <HeroSection
-        title="Nós"
+        title="Sobre Nós"
         subtitle="Somos uma solução para facilitar o acesso ao saneamento básico"
         paragraph="Em nossa plataforma será possível que você faça seu cadastro, informe
       o seu imóvel e agende visitas técnicas com nossos especialistas para
@@ -14,7 +14,7 @@ function About() {
       sua rede de saneamento básico,"
         span="sem dor de cabeça!"
       />
-    </section>
+    </>
   );
 }
 
