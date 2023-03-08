32873453fbc29b018d454f615ce436976769768f
Talita Santos <talita-s@ibm.com>
2023-03-08 12:54:00 -0300

    Ajsute para receber a fonte Quicksand

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import App from "./App";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import "@fontsource/quicksand/300.css";
+import "@fontsource/quicksand/500.css";
+import "@fontsource/quicksand/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
