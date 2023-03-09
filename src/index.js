18b1e6a6b3729e51d58c9d1a3f380766aee7e3da
Talita Santos <talita-s@ibm.com>
2023-03-09 12:51:32 -0300

    Inserindo o tema no site

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
 import App from "./App";
+
+import Theme from "./components/assets/Theme";
 
 import "@fontsource/quicksand/300.css";
 import "@fontsource/quicksand/500.css";
@@ -11,8 +14,10 @@ import "@fontsource/quicksand/700.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CssBaseline>
-      <App />
-    </CssBaseline>
+    <ThemeProvider theme={Theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>
 );
