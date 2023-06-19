68fa292aa5477d627352bea97099a9dc37928267
Talita-S <talitassantos2015@gmail.com>
2023-06-19 01:16:41 -0300

    Recuperando imóveis cadastrados

--- src/components/pages/Properties.js
+++ src/components/pages/Properties.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import axios from "axios";
 import { Link } from "react-router-dom";
-import { Grid, Button, Card, CardContent, Typography } from "@mui/material";
+import {
+  Grid,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import { css } from "@emotion/css";
 
 import NavigationBar from "../layout/NavigationBar";
 
 function Properties() {
+  const [properties, setProperties] = useState([]);
+  const token = localStorage.getItem("token");
+  const [open, setOpen] = React.useState(false);
+  const [selectedPropertyId, setSelectedPropertyId] = useState(null);
+
+  const handleClickOpen = (propertyId) => {
+    setSelectedPropertyId(propertyId);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDeleteProperty = () => {
+    setProperties((currentProperties) =>
+      currentProperties.filter((property) => property.id !== selectedPropertyId)
+    );
+
+    handleClose();
+  };
+
+  useEffect(() => {
+    fetch(
+      "http://localhost:30585/api/ResidentialProperty/get-user-properties",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => setProperties(json))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <NavigationBar />
@@ -41,43 +88,85 @@ function Properties() {
           flexWrap: "wrap",
         }}
       >
-        <Card
-          variant="outlined"
-          sx={{
-            minWidth: 250,
-            marginTop: 6,
-            bgcolor: "secondary.main",
-          }}
-        >
-          <CardContent>
-            <Typography sx={{ fontSize: 20 }} color="primary.main">
-              Imóvel RGI:
-            </Typography>
-            <Typography sx={{ fontSize: 16, color: "#666" }}>
-              Hidrômetro:
-            </Typography>
-            <Typography sx={{ fontSize: 14 }}>Rua</Typography>
-            <Typography sx={{ fontSize: 14 }}>Bairro - CEP</Typography>
-            <Typography sx={{ fontSize: 14 }}>Complemento</Typography>
-            <Typography sx={{ fontSize: 14 }}>Cidade, Estado</Typography>
-            <Grid
-              sx={{
-                marginTop: 2,
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-                justifyContent: "space-between",
-              }}
-            >
-              <Button variant="outlined" color="success">
-                Alterar
-              </Button>
-              <Button variant="outlined" color="error">
-                Excluir
-              </Button>
-            </Grid>
-          </CardContent>
-        </Card>
+        {!properties && (
+          <Typography sx={{ fontSize: 20 }} color="primary.main">
+            Você ainda não possui imóvel cadastrado.
+          </Typography>
+        )}
+        {properties.map((property) => (
+          <Card
+            variant="outlined"
+            sx={{
+              minWidth: 250,
+              marginTop: 6,
+              bgcolor: "secondary.main",
+            }}
+          >
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="primary.main">
+                Imóvel RGI: {property.rgi}
+              </Typography>
+              <Typography sx={{ fontSize: 16, color: "#666" }}>
+                Hidrômetro: {property.hidrometer}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }}>{property.street}</Typography>
+              <Typography sx={{ fontSize: 14 }}>
+                Bairro: {property.neighborhood} - CEP: {property.cep}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }}>
+                Complemento: {property.complement}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }}>
+                Cidade: {property.city}, {property.uf}
+              </Typography>
+              <Grid
+                sx={{
+                  marginTop: 2,
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Button variant="outlined" color="success">
+                  Alterar
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => handleClickOpen(property.id)}
+                >
+                  Excluir
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    {"Excluir imóvel"}
+                  </DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Deseja excluir o imóvel?
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Cancelar</Button>
+                    <Button
+                      color="error"
+                      onClick={handleDeleteProperty}
+                      autoFocus
+                    >
+                      Excluir
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </Grid>
+            </CardContent>
+          </Card>
+        ))}
       </Grid>
     </Grid>
   );
