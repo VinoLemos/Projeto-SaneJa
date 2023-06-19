1a217a19b45506838bf0a31d850bcf850d458ee1
Talita-S <talitassantos2015@gmail.com>
2023-06-18 23:37:15 -0300

    Função de cadastro de imóvel criada

--- src/components/pages/RegisterProperty.js
+++ src/components/pages/RegisterProperty.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
+import axios from "axios";
 import {
   Grid,
   Box,
@@ -14,6 +16,8 @@ import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
 import NavigationBar from "../layout/NavigationBar";
 import SubmitButton from "../layout/SubmitButton";
+import SuccessAlert from "../layout/SuccessAlert";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function RegisterProperty() {
   const {
@@ -23,7 +27,44 @@ function RegisterProperty() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token");
+
+  const onSubmit = async (data) => {
+    await axios
+      .post(
+        "http://localhost:30585/api/residentialproperty/register-property",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+        setLoading(true)
+      )
+      .then(() => {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+        setLoading(false);
+        setTimeout(() => {
+          navigate("/imoveis");
+        }, 5000);
+      })
+      .catch((error) => {
+        setError(true);
+        setTimeout(() => {
+          setError(false);
+        }, 3000);
+        setLoading(false);
+        console.log(error);
+      });
+  };
 
   const [loading, setLoading] = React.useState(false);
 
@@ -34,10 +75,10 @@ function RegisterProperty() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setValue("rua", data.street);
-        setValue("bairro", data.neighborhood);
-        setValue("cidade", data.city);
-        setValue("estado", data.state);
+        setValue("street", data.street);
+        setValue("neighborhood", data.neighborhood);
+        setValue("city", data.city);
+        setValue("uf", data.state);
         setLoading(false);
       });
   };
@@ -64,6 +105,10 @@ function RegisterProperty() {
           {loading && (
             <CircularProgress sx={{ color: "#3b8786", alignItems: "center" }} />
           )}
+          {success && <SuccessAlert message="Imóvel cadastrado com sucesso!" />}
+          {error && (
+            <ErrorAlert message="Ops, algo deu errado. Tente novamente mais tarde." />
+          )}
           <Box component="form" noValidate sx={{ mt: 1, maxWidth: "50vw" }}>
             <Grid container spacing={1} marginBottom={2}>
               <Grid item xs={12} sm={6}>
@@ -73,7 +118,7 @@ function RegisterProperty() {
                   fullWidth
                   label="CEP"
                   name="cep"
-                  type="text"
+                  type="number"
                   {...register("cep")}
                   onBlur={checkCEP}
                 />
@@ -87,9 +132,9 @@ function RegisterProperty() {
                   margin="normal"
                   fullWidth
                   label="Rua"
-                  name="rua"
+                  name="street"
                   type="text"
-                  {...register("rua")}
+                  {...register("street")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -98,15 +143,15 @@ function RegisterProperty() {
                   margin="normal"
                   fullWidth
                   label="Número"
-                  name="numero"
+                  name="number"
                   type="number"
-                  {...register("numero", {
+                  {...register("number", {
                     required: "Numero obrigatório",
                   })}
                 />
-                {errors.numero && (
+                {errors.number && (
                   <FormHelperText sx={{ color: "#bf6560" }}>
-                    {errors.numero.message}
+                    {errors.number.message}
                   </FormHelperText>
                 )}
                 <TextField
@@ -114,10 +159,28 @@ function RegisterProperty() {
                   margin="normal"
                   fullWidth
                   label="Bairro"
-                  name="bairro"
+                  name="neighborhood"
                   type="text"
-                  {...register("bairro")}
+                  {...register("neighborhood")}
                 />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  InputLabelProps={{ shrink: true }}
+                  margin="normal"
+                  fullWidth
+                  label="Complemento"
+                  name="complement"
+                  type="text"
+                  {...register("complement", {
+                    required: "Complemento obrigatório",
+                  })}
+                />
+                {errors.complement && (
+                  <FormHelperText sx={{ color: "#bf6560" }}>
+                    {errors.complement.message}
+                  </FormHelperText>
+                )}
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -125,18 +188,18 @@ function RegisterProperty() {
                   margin="normal"
                   fullWidth
                   label="Cidade"
-                  name="cidade"
+                  name="city"
                   type="text"
-                  {...register("cidade")}
+                  {...register("city")}
                 />
                 <TextField
                   InputLabelProps={{ shrink: true }}
                   margin="normal"
                   fullWidth
                   label="Estado"
-                  name="estado"
+                  name="uf"
                   type="text"
-                  {...register("estado")}
+                  {...register("uf")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -145,17 +208,31 @@ function RegisterProperty() {
                   fullWidth
                   label="RGI"
                   name="rgi"
-                  type="text"
-                  {...register("rgi")}
+                  type="number"
+                  {...register("rgi", {
+                    required: "RGI obrigatório",
+                  })}
                 />
+                {errors.rgi && (
+                  <FormHelperText sx={{ color: "#bf6560" }}>
+                    {errors.rgi.message}
+                  </FormHelperText>
+                )}
                 <TextField
                   margin="normal"
                   fullWidth
                   label="Hidrômetro"
-                  name="hidrometro"
-                  type="text"
-                  {...register("hidrometro")}
+                  name="hidrometer"
+                  type="number"
+                  {...register("hidrometer", {
+                    required: "Hidrômetro obrigatório",
+                  })}
                 />
+                {errors.hidrometer && (
+                  <FormHelperText sx={{ color: "#bf6560" }}>
+                    {errors.hidrometer.message}
+                  </FormHelperText>
+                )}
               </Grid>
             </Grid>
             <SubmitButton text="Cadastrar" onClick={handleSubmit(onSubmit)} />
