d998e4b5cd17ea079c45c100e65bccab94f136b7
Talita-S <talitassantos2015@gmail.com>
2023-06-18 23:30:35 -0300

    Componente de proxy criado

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://localhost:7021",
+      changeOrigin: true,
+    })
+  );
+};
