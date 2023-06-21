bebf0e39d0d5ff76e1a716a28ff82c379ae6839c
Talita-S <talitassantos2015@gmail.com>
2023-06-20 21:54:57 -0300

    Habilitando função de agendamento de visita técnica

--- src/components/pages/TechnicalVisit.js
+++ src/components/pages/TechnicalVisit.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../../env";
 import { useForm } from "react-hook-form";
 
 import {
@@ -11,10 +12,15 @@ import {
   TextField,
   Select,
   InputLabel,
+  MenuItem,
+  CircularProgress
 } from "@mui/material";
 
 import NavigationBar from "../layout/NavigationBar";
 import SubmitButton from "../layout/SubmitButton";
+import SuccessAlert from "../layout/SuccessAlert";
+import ErrorAlert from "../layout/ErrorAlert";
+import axios from "axios";
 
 function TechnicalVisit() {
   const {
@@ -23,7 +29,62 @@ function TechnicalVisit() {
     formState: { errors },
   } = useForm({ mode: "onBlur" });
 
-  const onSubmit = (data) => console.log(data);
+  const [properties, setProperties] = useState([]);
+  const [selectedPropertyId, setSelectedPropertyId] = useState('');
+  const [loading, setLoading] = React.useState(false);
+  const token = localStorage.getItem("token");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const statusId = "442ed696-b819-493f-aab8-b754104673c6";
+
+  useEffect(() => {
+    fetch(
+      `${API_URL}/ResidentialProperty/get-user-properties`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => { setProperties(json); if (json.length > 0) { setSelectedPropertyId(json[0].id) } })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const onSubmit = async (data) => {
+    const propertyId = localStorage.getItem("propertyId");
+    const requestVisitData = {
+      "residentialPropertyId": propertyId,
+      "statusId": statusId,
+      "visitDate": data.visitDate
+    }
+
+    await axios.post(`${API_URL}/technicalvisit/request-visit`, requestVisitData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      setLoading(true))
+      .then(() => {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+        setLoading(false);
+        setTimeout(() => {
+        }, 5000);
+      })
+      .catch((error) => {
+        setError(true);
+        setTimeout(() => {
+          setError(false);
+        }, 3000);
+        setLoading(false);
+        console.log(error);
+      });
+  };
 
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
@@ -32,6 +93,10 @@ function TechnicalVisit() {
         <Typography component="h1" variant="h5" mt={5} textAlign="center">
           Agende uma visita
         </Typography>
+        {success && <SuccessAlert message="Visita agendada com sucesso!" />}
+        {error && (
+          <ErrorAlert message="Ops, algo deu errado. Tente novamente mais tarde." />
+        )}
       </Grid>
       <Box component="form" noValidate sx={{ mt: 1, textAlign: "center" }}>
         <FormControl sx={{ width: "30ch" }}>
@@ -48,26 +113,36 @@ function TechnicalVisit() {
           <TextField
             fullWidth
             margin="normal"
-            name="dataVisita"
+            name="visitDate"
             type="date"
-            {...register("dataVisita", {
+            {...register("visitDate", {
               required: "Data obrigatória",
             })}
           />
-          {errors.dataVisita && (
+          {errors.visitDate && (
             <FormHelperText sx={{ color: "#bf6560" }}>
-              {errors.dataVisita.message}
+              {errors.visitDate.message}
             </FormHelperText>
           )}
           <FormControl fullWidth>
             <InputLabel>Selecione o imóvel</InputLabel>
+
             <Select
+              value={selectedPropertyId}
+              onChange={(event) => setSelectedPropertyId(event.target.value)}
               label="Selecione o imóvel"
               sx={{ marginBottom: "1em" }}
-            ></Select>
+            >
+              {properties.map((property) => (
+                localStorage.setItem("propertyId", property.id),
+                <MenuItem value={property.id} key={property.id}>{property.street}</MenuItem>
+              ))}
+
+            </Select>
+
           </FormControl>
 
-          <SubmitButton text="Agendar" onClick={handleSubmit(onSubmit)} />
+          <SubmitButton text={loading ? <CircularProgress sx={{ color: "#3b8786", alignItems: "center" }} /> : "Agendar"} onClick={handleSubmit(onSubmit)} />
         </FormControl>
       </Box>
     </Grid>
